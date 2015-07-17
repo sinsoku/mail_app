785ee229984f133bdf3c5ea8f2ee096fa956b420
sinsoku <sinsoku.listy@gmail.com>
2015-07-17 15:15:08 +0900

    Update setting to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port: ENV['MAILGUN_SMTP_PORT'],
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
     domain: 'heroku.com',
-    enable_starttls_auto: true
   }
 end
